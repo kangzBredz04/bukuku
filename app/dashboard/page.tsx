be2d7b15f9adcfb49a4b9e1e44e3bae03064accf
kangzBredz04 <wabredz1234@gmail.com>
2024-08-10 17:56:25 +0700

    update project

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,19 @@
+"use client";
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 // `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
 export default function Page() {
+  const router = useRouter();
   return (
     <>
       <h1>Hello, Dashboard Page!</h1>
-      <Link href={"/"}>To Home</Link>
+      <Link href={"/"}>To Home (Menggunakan Link)</Link>
+
+      <button type="button" onClick={() => router.push("/")}>
+        To Home (Menggunakan useRouter)
+      </button>
     </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
+"use client";
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 // `app/page.tsx` is the UI for the `/` URL
 export default function Page() {
+  const router = useRouter();
+
   return (
     <>
       <h1>Hello, Home page!</h1>
-      <Link href={"/dashboard"}>To Dashboard</Link>
+      <Link href={"/dashboard"}>To Dashboard (Menggunakan Link)</Link>
+
+      <button type="button" onClick={() => router.push("/dashboard")}>
+        To Dashboard (Menggunakan useRouter)
+      </button>
     </>
   );
 }
